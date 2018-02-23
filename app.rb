68723e0c6fd923b590275d75a8dbd11a0aa738ff
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-02-22 19:25:27 -0800

    fill home, list success erb with html

--- app.rb
+++ app.rb
@@ -2,16 +2,26 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**.*.rb')
 require('./lib/task')
+require('./lib/list')
 require('pg')
 require('pry')
 
 DB = PG.connect({:dbname => 'to_do'})
 
-
+# dat home
 get('/') do
-  erb(:input)
+  erb(:home)
 end
 
-get('/output') do
-  erb(:output)
+# dat list form page
+get('/lists/new') do
+  erb(:list_form)
+end
+
+# Fetch new list name from parameter. Create new list object. id will remain nil until assigned by DB. Save list. Render success page
+post('/lists') do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:list_success)
 end
