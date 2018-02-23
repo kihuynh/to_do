1ad8c323e5cec1ab3b831678f4381d736f7044ef
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-02-22 21:28:39 -0800

    lost track. adding inte spec and stuff into list and lists erb

--- app.rb
+++ app.rb
@@ -6,19 +6,28 @@ require('./lib/list')
 require('pg')
 require('pry')
 
-DB = PG.connect({:dbname => 'to_do'})
+DB = PG.connect({:dbname => 'to_do_test'})
 
 # dat home
 get('/') do
   erb(:home)
 end
 
+get('/lists') do
+  @lists = List.all()
+  erb(:lists)
+end
+
+get('/lists/:id') do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list)
+end
 # dat list form page
 get('/lists/new') do
   erb(:list_form)
 end
 
-# Fetch new list name from parameter. Create new list object. id will remain nil until assigned by DB. Save list. Render success page
+# Fetch new list name from parameter. Create new list object with fetch info. id will remain nil until assigned by DB. Save list. Render success page
 post('/lists') do
   name = params.fetch("name")
   list = List.new({:name => name, :id => nil})
--- lib/list.rb
+++ lib/list.rb
@@ -22,6 +22,28 @@ class List
     @id = result.first().fetch("id").to_i()
   end
 
+  # don't completely understand how this one works
+  def self.find(id)
+    found_list = nil
+    List.all().each() do |list|
+      if list.id().==(id)
+        found_list = list
+      end
+    end
+    found_list
+  end
+
+  def tasks
+    list_tasks = []
+    tasks = DB.exec("SELECT * FROM tasks where list_id = #{self.id()};")
+    tasks.each() do |task|
+      description = task.fetch("description")
+      list_id = task.fetch("list_id").to_i()
+      list_tasks.push(Task.new({:description => description, :list_id => list_id}))
+    end
+    list_tasks
+  end
+
   def ==(another_list)
     self.name().==(another_list.name()).&(self.id().==(another_list.id()))
   end
