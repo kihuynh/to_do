8751341ae75ff0b2c044b5107b3c829c45320366
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-02-22 16:16:40 -0800

    spec to save for list rb. test and pass.

--- lib/list.rb
+++ lib/list.rb
@@ -1,5 +1,11 @@
-class Lists
+class List
+  attr_reader(:name, :id)
 
+  def initialize(attributes)
+    name = attributes.fetch(:name)
+  end
 
-
+  def ==(another_list)
+    self.name().==(another_list.name()).&(self.id().==(another_list.id()))
+  end
 end
